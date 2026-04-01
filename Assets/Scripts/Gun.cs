e311b328d24f33acd72cb845855dd560e2fe3d8e
N8tivegamer <gamerboz652@gmail.com>
2026-04-01 10:46:41 -0500

    new Gun Scripts

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float reloadTime = 1f;
+    public float fireRate = 0.15f;
+    public int magSize = 20;
+
+    public GameObject bullet;
+    public GameObject bulletSpawnPoint;
+
+    private int currentAmmo;
+    private bool isReloading = false;
+    private float nextTimeToFire = 0f;
+
+    private Quaternion initalRotation;
+    private Vector3 initalPosition;
+    private Vector3 reloadRotationOffset = new Vector3(66, 50, 50);
+
+
+
     void Start()
     {
-        
+        currentAmmo = magSize;
+        initalRotation = transform.localRotation;
+        initalPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shoot()
     {
-        
+
     }
 }
