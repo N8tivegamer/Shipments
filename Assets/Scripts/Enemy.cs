2d18676f5d9ce1865c4748a74a5e032ea5234bff
N8tivegamer <gamerboz652@gmail.com>
2026-04-02 14:12:34 -0500

    Pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,26 @@
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    private Transform target;  // Storage for the Player's position
+    private NavMeshAgent ai; // Reference to the AI component on this object
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Link the 'ai' variable to the NavMeshAgent component attached to this GameObject
+        ai = GetComponent<NavMeshAgent>();
+
+        // Locate the Player by looking for the "Player" tag and grab its Transform
+        target = GameObject.FindWithTag("Player") .transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Tell the NavMeshAgent to calculate a path and move toward the player's current position
+        ai.SetDestination(target.position);
     }
 }
